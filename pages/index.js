cd84530aac2d0aaf2d9e65d2d38b57d19626c981
Joyal S Palackel <joyal.palackel19@gmail.com>
2021-06-28 13:10:34 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,55 @@
+import { getSession } from "next-auth/client";
+import Head from 'next/head'
+import Header from '../components/Header'
+import Login from './../components/Login';
+import Sidebar from './../components/Sidebar';
+import Feed from './../components/Feed';
+import Widgets from './../components/Widgets';
+import { db } from "../firebase";
+
+export default function Home({ session, posts }) {
+  
+  if(!session) return <Login />
+  return (
+    <div className="h-screen bg-gray-100 overflow-hidden">
+      <Head>
+        <title>Facebook 2.0</title>
+      </Head>
+
+
+      {/* Header */}
+      <Header />
+
+      <main className="flex">
+         {/* sidebar */}
+         <Sidebar />
+         
+         {/* Feed */}
+         <Feed posts={posts} />
+         {/* Widgets */}
+         <Widgets />
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+
+  const session = await getSession(context);
+
+  const posts = await db.collection("posts").orderBy("timestamp", "desc").get();
+  const docs = posts.docs.map(post => ({
+    id: post.id,
+    ...post.data(),
+    timestamp: null
+  }))
+
+  
+  
+  return {
+    props: {
+      session,
+      posts: docs,
+    }, 
+  };
+}
